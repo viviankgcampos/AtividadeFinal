261f9dc3513356f5c51a4a76f5d79d076904df0a
viviankgcampos <vivian@unoeste.edu.br>
2023-06-12 22:24:05 -0300

    Subindo commit da aplicacao

---
+++ app.js
@@ -0,0 +1,73 @@
+﻿const express = require("express");
+const os = require("os");
+const app = express();
+
+
+app.get("/", (request, response) => {
+    return response
+        .status(200)
+        .json({
+            message: "Olá"
+        });
+});
+
+app.get("/liveness", (request, response) => {
+    return response
+        .status(200)
+        .json({
+            message: "Meu app está vivo!",
+            path: process.cwd(),
+            gid: process.getegid(),
+            uid: process.geteuid(),
+            date: new Date().getTime()
+        });
+});
+
+app.get("/readiness", (request, response) => {
+    //CACHE (Redis)
+    //BANCO DE DADOS (MySQL, Postgre, MongoDB)
+    //CLIMA TEMPO
+
+    return response
+        .status(200)
+        .json({
+            message: "Meu app está pronto!",
+            platform: os.platform(),
+            freemem: os.freemem(),
+            homedir: os.homedir(),
+            date: new Date().getTime()
+        });
+});
+
+
+
+app.get("/consulta-dados", (request, response) => {
+
+    //SCRIPT CREATE DATABASE
+    //SCRIPT TABLE
+    //SCRIPT INSERT
+
+    //GITHUB
+    //DOCKERHUB
+
+
+    //listagem dos cadastros da tabela criada.
+
+    //conexao com banco
+    //instalacao do pacote npm
+
+
+    return response
+        .status(200)
+        .json({
+            message: "Meu app está pronto!",
+            platform: os.platform(),
+            freemem: os.freemem(),
+            homedir: os.homedir(),
+            date: new Date().getTime()
+        });
+});
+
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,10 @@
+﻿const http = require("http");
+const app = require("./app");
+
+const port = 3000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log("server is running on port " + port);
+});
